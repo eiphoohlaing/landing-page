50e086e0f8437b108c104755606465a9319540d1
eiphoohlaing <jisung5588589@gmail.com>
2023-02-15 09:53:58 +0630

    fixed css

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,7 @@
 const darkSwitchIcon = document.querySelector("#dark-switch-icon");
 const darkSwitch = document.querySelector("#dark-switch")
 const darkText = document.querySelector("#dark-text")
-// const darkChangeText = document.querySelector("#dark-text-change")
+const darkChangeText = document.querySelector("#dark-text-change")
 const html = document.documentElement;
 let isDarkMode = false;
 
@@ -12,7 +12,7 @@ const toggleTheme = () => {
 }
 
 const toDark = () => {
-    darkSwitchIcon.classList.add('translate-x-full', 'rotate-[360deg]','bg-slate-900')
+    darkSwitchIcon.classList.add( 'rotate-[360deg]','bg-slate-900')
     darkSwitchIcon.innerHTML = `<i class="fa-solid fa-moon text-slate-100"></i>`
     // darkChangeText.innerText = 'Dark'
     darkSwitch.classList.remove('bg-slate-800')
@@ -31,7 +31,7 @@ const toLight = () => {
     localStorage.removeItem('data-theme')
     html.classList.remove('dark')
     darkText.classList.remove('-translate-x-7')
-    darkText.innerText = 'OFF'
+    darkText.innerText = 'FF'
     darkSwitchIcon.innerHTML = `<i class="fa-regular fa-sun"></i>`
     setTimeout(() => {
         darkSwitchIcon.classList.remove('rotate-[360deg]')
